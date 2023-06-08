30c277d6fdd6982c58bd8b802cd6db89d772aa34
Khatri-Jinal <jinal.khatri@simformsolutions.com>
2023-06-08 14:40:02 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server is a running on port ${port}`);
+});
